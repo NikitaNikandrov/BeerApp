17e722bde44867680f501e64733fac524f14c1a1
NikitaNikandrov <nnikandroff@yandex.ru>
2023-07-11 00:05:24 +0300

    1. NetworkServiceResources folder renamed to Resources 2. Created NetworkServiceParameters enum

---
+++ BeerApp/Resources/Resources.swift
@@ -0,0 +1,29 @@
+//
+//Resources.swift
+//  BeerApp
+//
+//  Created by Никита on 10.07.2023.
+//
+
+import Foundation
+
+enum NetworkServiceStrings {
+    static var rootEndpoint = "https://api.punkapi.com/v2/"
+}
+
+enum NetworkServiceParameters {
+    static var abvGreater = "abv_gt"
+    static var abvLess = "abv_lt"
+    static var ibuGreater = "ibu_gt"
+    static var ibuLess = "ibu_lt"
+    static var ebcGreater = "ebc_gt"
+    static var ebcLess = "ebc_lt"
+    static var beerName = "beer_name"
+    static var yeast = "yeast"
+    static var brewedBefore = "brewed_before"
+    static var brewedAfter = "brewed_after"
+    static var hops = "hops"
+    static var malt = "malt"
+    static var food = "food"
+    static var ids = "ids"
+}
