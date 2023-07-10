5147339c30c854fa59a2efaeaf59d049fd3bffbe
NikitaNikandrov <nnikandroff@yandex.ru>
2023-07-10 23:53:54 +0300

    1. Created NetworkServiceStrings file, enum 2. Added root url

---
+++ BeerApp/Data/NetworkServiceStrings.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkServiceStrings.swift
+//  BeerApp
+//
+//  Created by Никита on 10.07.2023.
+//
+
+import Foundation
+
+enum NetworkServiceStrings {
+    static var rootEndpoint = "https://api.punkapi.com/v2/"
+}
+
+enum NetworkServiceParameters {
+    
+}
