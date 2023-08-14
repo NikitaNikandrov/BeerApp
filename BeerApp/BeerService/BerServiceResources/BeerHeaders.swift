b8bdd2f54d4248e770979f0f357f275855d03554
NikitaNikandrov <nnikandroff@yandex.ru>
2023-08-14 23:34:43 +0300

    feature BeerServiceResources in BeerService

---
+++ BeerApp/BeerService/BerServiceResources/BeerHeaders.swift
@@ -0,0 +1,25 @@
+//
+//  BeerHeaders.swift
+//  BeerApp
+//
+//  Created by Никита on 14.08.2023.
+//
+
+import Foundation
+
+enum BeerHeaders {
+    static let abvGt = "abv_gt"
+    static let abvLt = "abv_lt"
+    static let ibuGt = "ibu_gt"
+    static let ibuLt = "ibu_lt"
+    static let ebcGt = "ebc_gt"
+    static let ebcLt = "ebc_lt"
+    static let beerName = "beer_name"
+    static let yeast = "yeast"
+    static let brewedBefore = "brewed_before"
+    static let brewedAfter = "brewed_after"
+    static let hops = "hops"
+    static let malt = "malt"
+    static let food = "food"
+    static let ids = "ids"
+}
---
+++ BeerApp/BeerService/BerServiceResources/BeerPath.swift
@@ -0,0 +1,14 @@
+//
+//  BeerPath.swift
+//  BeerApp
+//
+//  Created by Никита on 14.08.2023.
+//
+
+import Foundation
+
+enum BeerPath {
+    static let getBeers = "beers"
+    static let getRandomBeer = "beers/random"
+    static let getSingle = "beers/"
+}
